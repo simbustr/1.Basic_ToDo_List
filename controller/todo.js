a01162cd01d46be580dcdfc453c1c0d5d6e61255
silambarasan <hellosimbu2023@gmail.com>
2024-04-21 17:37:03 +0530

    todolist

--- controller/todo.js
+++ controller/todo.js
@@ -1,30 +1,63 @@
 const express = require("express");
 const todoModel = require("../model/todo");
 
-
-// Define the individual route handlers
 exports.createTodo = async (req, res) => {
-    // Function logic for creating a new todo
-    const { title, description } = req.body;
-    try {
-      const newTodo = new todoModel({ title, description });
-      await newTodo.save();
-      res.status(201).json(newTodo);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: error.message });
-    }
-  };
-  
-  exports.getAllTodos = async (req, res) => {
-    // Function logic for getting all todos
-    try {
-      const todos = await todoModel.find();
-      res.json(todos);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: error.message });
-    }
-  };
+  const { title, description } = req.body;
+  try {
+    const newTodo = new todoModel({ title, description });
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
 
+exports.getAllTodos = async (req, res) => {
+  try {
+    const todos = await todoModel.find();
+    res.json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
 
+exports.updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  try {
+    const updateTodo = await todoModel.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    res.json(updateTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedTodo = await todoModel.findByIdAndDelete(id);
+
+    if (!deletedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    res.json({ message: "Todo deleted successfully", deletedTodo });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/todo.js
+++ routes/todo.js
@@ -8,4 +8,13 @@ router.post("/", todoController.createTodo);
 // Get all items
 router.get("/", todoController.getAllTodos);
 
+
+// Update a todo item
+router.put("/:id", todoController.updateTodo);
+
+
+
+// Delete a todo item
+router.delete("/:id", todoController.deleteTodo);
+
 module.exports = router;
